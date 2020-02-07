ef470afa1c37de3ee31f0bd52f1052bdd4226511
Larko_0 <larionov1001@gmail.com>
2020-02-07 21:48:08 +0200

    Added models: Card + Card_class + Ability

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Ability(models.Model):
+    name = models.CharField(max_length=50, default='')
+    description = models.TextField(max_length=500, default='')
+
+
+class CardClass(models.Model):
+    name = models.CharField(max_length=50, default='')
+    description = models.TextField(max_length=500, default='')
+
+
+class Card(models.Model):
+    name = models.CharField(max_length=50, default="")
+    mana = models.IntegerField(default=0)
+    attack = models.IntegerField(default=0)
+    hp = models.IntegerField(default=0)
+    abilities = models.ManyToManyField(Ability, blank=True, null=True)
+    card_class = models.ForeignKey(CardClass, blank=True, null=True, on_delete=models.SET_NULL)
+    img = models.CharField(max_length=100, default='')
